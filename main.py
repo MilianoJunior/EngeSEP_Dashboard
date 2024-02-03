abec2e17527df4eb9a2cf8b43d21128cce71a048
MilianoJunior <jrmfilho37@gmail.com>
2024-02-03 17:18:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+    Author: Miliano Fernandes de Oliveira Junior
+    Date: 03/02/2024
+    Description: Este é um projeto que vai estar no github e será vinculado ao servidor da railway, sendo assim, o
+    projeto será um dashboard que vai mostrar os dados de um banco de dados que será alimentado por varias tabelas
+    que já estão no servidor da railway.
+
+    1 passo: Criar um projeto no github e vincular ao projeto local.
+    2 passo: Vincular o repositório do github ao servidor da railway.
+'''
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
